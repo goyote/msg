9a6e8d35d650bfa381296c595d730a77dcd1dc1d
Gregorio Ramirez <drillbugs@gmail.com>
2011-02-06 16:12:36 -0800

    MSG shoudn't escape by default.

--- views/msg/all.php
+++ views/msg/all.php
@@ -1,7 +1,7 @@
 <ul id="messages">
 	<?php foreach ($messages as $message): ?>
-		<li class="<?php echo HTML::chars($message['type']) ?>">
-			<p><?php echo HTML::chars($message['text']) ?></p>
+		<li class="<?php echo $message['type'] ?>">
+			<p><?php echo $message['text'] ?></p>
 		</li>
 	<?php endforeach ?>
 </ul>
