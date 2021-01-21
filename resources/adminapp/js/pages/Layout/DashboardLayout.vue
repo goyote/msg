b0d8b4111da289f7d268ea7f093050b37d9c55f1
Laravel Daily <info@laraveldaily.com>
2021-01-21 23:28:47 +0000

    QuickAdminPanel automatic commit

---
+++ resources/adminapp/js/pages/Layout/DashboardLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <event-hub></event-hub>
+    <side-bar :sidebarLinks="sidebarLinks">
+      <mobile-menu slot="content"></mobile-menu>
+    </side-bar>
+
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+      <div class="content">
+        <dashboard-content></dashboard-content>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DashboardContent from './Content.vue'
+import TopNavbar from './TopNavbar.vue'
+import MobileMenu from './MobileMenu.vue'
+
+export default {
+  components: {
+    DashboardContent,
+    TopNavbar,
+    MobileMenu
+  },
+  data() {
+    return {
+      sidebarLinks: [
+        {
+          title: 'global.dashboard',
+          icon: 'dashboard',
+          path: { name: 'dashboard' }
+        },
+        {
+          title: 'cruds.userManagement.title',
+          icon: 'person',
+          path: { name: 'user_management' },
+          gate: 'user_management_access',
+          children: [
+            {
+              title: 'cruds.permission.title',
+              icon: 'perm_data_setting',
+              path: { name: 'permissions.index' },
+              gate: 'permission_access'
+            },
+            {
+              title: 'cruds.role.title',
+              icon: 'group',
+              path: { name: 'roles.index' },
+              gate: 'role_access'
+            },
+            {
+              title: 'cruds.user.title',
+              icon: 'person',
+              path: { name: 'users.index' },
+              gate: 'user_access'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
